890cbefe53dde1029c29f7a6619051523e53ec0b
jacer7 <jsor.khadka@gmail.com>
2024-12-03 08:09:32 +0100

    added tfvars

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region                   = var.aws_region
-  profile                  = "alawme"
-  shared_credentials_files = ["~/.aws/credentials"]
+  # profile                  = "alawme"
+  # shared_credentials_files = ["~/.aws/credentials"]
 }
 
 terraform {
